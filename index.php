ec6fdb068ea35e513260156dcf541536820a6b8a
davidmoina <luisdavidmoina@gmail.com>
2023-02-01 17:58:39 +0100

    modified image url

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-    $urlImages = "/ofteFQqIntae7wqVAYYyXOCmkF1.jpg";
+    $urlImages = "https://image.tmdb.org/t/p/w500";
+    $urlFetch = "https://api.themoviedb.org/3/movie/popular?api_key=f97d6a2165e719275828bcd71a17fccc&language=en-US&page=1";
 ?>
