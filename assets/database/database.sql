87135e137b3bbc449538bf81ef83e25c6c20485d
Alvaro Sanchez <alvarosanro89@gmail.com>
2023-02-01 18:51:08 +0100

    add diagram and tables

---
+++ assets/database/database.sql
@@ -0,0 +1,64 @@
+DROP DATABASE IF EXISTS Neflis;
+CREATE DATABASE IF NOT EXISTS Neflis;
+USE Neflis;
+
+CREATE TABLE  movies (
+    id int NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    language CHAR(2) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    poster_path VARCHAR(255) NOT NULL,
+    release_date DATE NOT NULL,
+    vote_average FLOAT NOT NULL,    
+    vote_count INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    rol ENUM('A','U')  NOT NULL,  
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,  
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE list_user_movies (
+    id INT NOT NULL AUTO_INCREMENT,
+    id_user INT NOT NULL, 
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_user) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE movies_in_list (
+    id INT NOT NULL AUTO_INCREMENT,
+    id_list INT NOT NULL,
+    id_movie INT NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,  
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_list) REFERENCES list_user_movies (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_movie) REFERENCES movies (id) ON DELETE CASCADE
+);
+
+CREATE TABLE comments (
+    id INT NOT NULL AUTO_INCREMENT,
+    id_user INT NOT NULL,
+    id_movie INT NOT NULL,
+    text text(500) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_user) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_movie) REFERENCES movies (id) ON DELETE CASCADE
+);
+
+CREATE TABLE likes (
+    id INT NOT NULL AUTO_INCREMENT,
+    id_user INT NOT NULL,
+    id_movie INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_user) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_movie) REFERENCES movies (id) ON DELETE CASCADE
+);
+
+
