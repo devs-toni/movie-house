8c556ffd858f8a128ef11d2c70a1dd5f0e37763b
Isaura Martí <imarti.teixido@gmail.com>
2023-02-02 18:28:59 +0100

    change styles modals

--- src/templates/modalLogin.php
+++ src/templates/modalLogin.php
@@ -1,20 +1,22 @@
 <dialog open class="modal">
-    <button class="modal__btnClose">🗙</button>
-    <h2 class="modal__title">Login</h2>
-    <form method="dialog" class="modal__form">
-        <div class>
-            <label class="form__label" for="email">Email</label>
-            <input id="email" type="text">
-        </div>
+    <div class="modal__container">
+        <button class="modal__btn-close">🗙</button>
+        <h2 class="modal__title">LOGIN</h2>
+        <form method="dialog" class="modal__form modal__form--login">
+            <div class>
+                <label class="form__label" for="email">Email</label>
+                <input class="form__input" id="email" type="text">
+            </div>
 
+            <div>
+                <label class="form__label" for="password">Password</label>
+                <input class="form__input" id="password" type="password">
+            </div>
+
+            <button type="submit" class="modal__btn-submit modal__btn-submit--login">Login</button>
+        </form>
         <div>
-            <label class="form__label" for="password">Password</label>
-            <input id="password" type="text">
+            <p class="modal__p modal__p--login">Need an account? <span>SIGN UP</span></p>
         </div>
-
-        <button type="submit">Login</button>
-    </form>
-    <div>
-        <p>Need an account? <span>Sign UP</span></p>
     </div>
 </dialog>
--- src/templates/modalSignUp.php
+++ src/templates/modalSignUp.php
@@ -1,20 +1,22 @@
 <dialog open class="modal-sign-up modal">
-    <button id="closeSignUp">🗙</button>
-    <h2 class="modal__title">SIGN UP</h2>
-    <form method="dialog">
-        <div>
-            <label class="form__label" id="username-sign-up">Username</label>
-            <input type="text" name="username-sign-up" id="username-sign-up">
-        </div>
-        <div>
-            <label class="form__label" id="email-sign-up">Email</label>
-            <input type="text" name="email-sign-up" id="email-sign-up">
-        </div>
-        <div>
-            <label class="form__label" id="password-sign-up">Password</label>
-            <input type="password" name="password-sign-up" id="password-sign-up">
-        </div>
-        <button type="submit">Sign Up</button>
-    </form>
-    <p>Already a user?<span>LOGIN</span></p>
+    <div class="modal__container">
+        <button id="closeSignUp" class="modal__btn-close">🗙</button>
+        <h2 class="modal__title">SIGN UP</h2>
+        <form method="dialog" class="modal__form">
+            <div>
+                <label class="form__label" id="username-sign-up">Username</label>
+                <input class="form__input" type="text" name="username-sign-up" id="username-sign-up">
+            </div>
+            <div>
+                <label class="form__label" id="email-sign-up">Email</label>
+                <input class="form__input" type="text" name="email-sign-up" id="email-sign-up">
+            </div>
+            <div>
+                <label class="form__label" id="password-sign-up">Password</label>
+                <input class="form__input" type="password" name="password-sign-up" id="password-sign-up">
+            </div>
+            <button class="modal__btn-submit">Sign Up</button>
+        </form>
+        <p class="modal__p">Already a user? <span>LOGIN</span></p>
+    </div>
 </dialog>
