4db36c5d177dfa07e9ff9e8dd5af5b4e2175d5cd
davidmoina <luisdavidmoina@gmail.com>
2023-02-03 15:08:23 +0100

    added films container files

--- src/templates/allFilms.php
+++ src/templates/allFilms.php
@@ -1,11 +1,11 @@
-<section>
-    <h3>Catalogue</h3>
-    <div>
+<section class="all-films">
+    <h3 class="all-films__title">Catalogue</h3>
+    <div class="all-films__container">
     <?php 
-    $allPoster= $db->getAllFilms();
-    foreach($allPoster as $poster){
-        echo "<img src='". $urlImages. $poster."'>";
-    }
+        $allPoster= $db->getAllFilms();
+        foreach($allPoster as $poster){
+            echo "<img class='container__img' src='". $urlImages. $poster."'>";
+        }
     ?>
     </div>
 </section>
