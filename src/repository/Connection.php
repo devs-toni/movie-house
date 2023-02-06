9a4091803c3ecf27fa29c6b62cd1272f636fc346
Isaura Martí <imarti.teixido@gmail.com>
2023-02-06 10:00:12 +0100

    fix bugs

--- src/repository/Connection.php
+++ src/repository/Connection.php
@@ -1,11 +1,14 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/' . explode('/', $_SERVER['REQUEST_URI'])[1] . '/config.php');
+// require_once($_SERVER['DOCUMENT_ROOT'] . '/' . explode('/', $_SERVER['REQUEST_URI'])[1] . '/config.php');
+require_once('dbConfig.php');
 
-class Connection {
+class Connection
+{
 
   protected $con;
 
-  public function connect() {
-    $this->con = mysqli_connect(DB,USER,PASS,DB_NAME);
-  }  
+  public function connect()
+  {
+    $this->con = mysqli_connect(DB, USER, PASS, DB_NAME);
+  }
 }
