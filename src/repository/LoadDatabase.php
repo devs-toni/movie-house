d649b2eedb96b07ed29002c6d3f7c8b18b51ffa8
Isaura Martí <imarti.teixido@gmail.com>
2023-02-07 10:31:18 +0100

    change variable loadDatabase

--- src/repository/LoadDatabase.php
+++ src/repository/LoadDatabase.php
@@ -1,7 +1,7 @@
 <?php
 require_once('Repository.php');
 require_once('../models/Movie.php');
-require_once(str_replace('\\' , '/',__DIR__) . '/../../config.php');
+require_once(str_replace('\\', '/', __DIR__) . '/../../config.php');
 
 $db = new Repository();
 $films = json_decode($_POST['films']);
@@ -10,8 +10,9 @@ $db->deleteFilms();
 
 for ($i = 0; $i < count($films); $i++) {
   $f = $films[$i];
+  $posterPath = API_IMAGE_URL . $f->poster_path;
   if (!$f->adult) {
-    $movie = new Movie($f->title, $f->original_language, $f->overview, API_IMAGE_URL . $f->poster_path, isset($f->release_date) ? $f->release_date : null, $f->vote_average);
+    $movie = new Movie($f->title, $f->original_language, $f->overview, $posterPath, isset($f->release_date) ? $f->release_date : null, $f->vote_average);
     $db->addFilm($movie);
   }
 }
