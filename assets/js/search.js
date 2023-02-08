fb6d0524cbabeb7ade150438ea2389ea958b24db
Antonio Rufino <devs.toni@gmail.com>
2023-02-08 20:39:53 +0100

    solve search front bug

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,8 +8,10 @@ searchInput && searchInput.addEventListener("keyup", searchMovies);
 function searchMovies() {
   let movie = searchInput.value;
   const paginationContainer = document.getElementById("paginationContainer");
-  const top10 = document.querySelector(".top-10");
-  top10.style.display = 'none';
+  const trending = document.querySelectorAll(".top-10");
+  trending.forEach(c => {
+    c.style.display = "none";
+  });
   if (movie.length >= 2) {
     title ? title.textContent = "Search Results" : "";
     fetch("src/controllers/Search.php?schMovies=" + movie, {
@@ -17,23 +19,25 @@ function searchMovies() {
     })
       .then((res) => res.json())
       .then((data) => {
-            if(!adminPage){
-            paginationContainer.style.visibility = "hidden";
-            printFilms(data, '#paginatedList');
-            }else{
-                searchEditFilms(data);
-            }
+        if (!adminPage) {
+          paginationContainer.style.visibility = "hidden";
+          printFilms(data, '#paginatedList');
+        } else {
+          searchEditFilms(data);
+        }
       });
   }
 
   if (movie.length == 0) {
     title ? title.textContent = "Catalogue" : "";
-        if(!adminPage){
-        top10.style.display = "flex";
-        setCurrentPage(currentPage);
-        paginationContainer.style.visibility = "visible";
-        }else{
-            tableBody.innerHTML = "" ;
-        }
+    if (!adminPage) {
+      trending.forEach(c => {
+        c.style.display = "flex";
+      });
+      setCurrentPage(currentPage);
+      paginationContainer.style.visibility = "visible";
+    } else {
+      tableBody.innerHTML = "";
+    }
   }
 }
