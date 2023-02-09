373c388c0b62c574f22daafe40c42381bc267785
davidmoina <luisdavidmoina@gmail.com>
2023-02-09 20:56:26 +0100

    fixed pagination bug

--- assets/js/returnIndex.js
+++ assets/js/returnIndex.js
@@ -1,11 +1,11 @@
-window.addEventListener("load", checkPageToReturn());
+window.addEventListener("load", checkPageToReturn);
 
 function checkPageToReturn() {
   fetch("src/controllers/CheckPageToReturn.php", { method: "GET" })
     .then((res) => res.json())
     .then((data) => {
       if (data !== "noData") {
-        setCurrentPage(data);
+        setCurrentPage(parseInt(data));
       }
     });
 }
