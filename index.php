8618249e34ffd6944ac944629c3591232b4bed08
Antonio Rufino <antonio.rufino@nexiona.com>
2024-05-02 20:52:51 +0200

    internal: update project

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $dbMovies = new MovieRepository();
 $dbUsers = new UserRepository();
 $isLogged = false;
 $isAdmin = false;
-$isAdult = false;
+$isAdult = true;
 $sections = ['history', 'thriller', 'action', 'comedy', 'adventure', 'horror', 'drama', 'crime', 'fantasy', 'documentary', 'science_fiction', 'western', 'mystery', 'music', 'romance', 'family', 'war'];
 
 // USER LOGIN
